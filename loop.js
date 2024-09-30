7c5f016ef55069c72b14eea776ef2c18dd85a3cf
uki <dinushan1129@gmail.com>
2024-09-30 09:43:52 +0530

    first loops

---
+++ loop.js
@@ -0,0 +1,25 @@
+const arr =[10,20,30,5]
+let sum=0;
+let i=0;
+while (i < Array.length) {
+    sum += arr[i];
+    i++;
+}
+console.log(sum);
+
+const boom =[10,20,30,5]
+let summ=0;
+let j=0;
+for (let j = 0; j < boom.length; j++) {
+    summ += boom[j];
+}
+console.log(summ)
+
+const arr1 =[10,20,30,5]
+let sum1=0;
+let k=0;
+do {
+    sum1 += arr1[k];
+    k++;
+} while (k < arr1.length);
+console.log(sum1)
